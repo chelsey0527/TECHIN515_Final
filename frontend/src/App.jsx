42ff9ef2281607441b78186b1dea55ed5f5b2ec8
Chelsey Lee <chelsey0527@gmail.com>
2024-05-10 14:41:30 -0700

    update frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import NavigationBar from "./components/NavigationBar";
 import Dashboard from "./pages/Dashboard";
+import IntakeHistory from "./pages/IntakeHistory";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
-    <main className=" h-screen flex ">
-      <NavigationBar />
-      <div>
-        <Dashboard />
-      </div>
-    </main>
+    <BrowserRouter>
+      <main className="h-screen flex">
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/intake-history" element={<IntakeHistory />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/IntakeHistory.jsx
@@ -0,0 +1,3 @@
+export default function IntakeHistory() {
+  return <div>IntakeHistory</div>;
+}
