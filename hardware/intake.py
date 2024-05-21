2bb196f6c51c224aa982c5ef74fff12058e0e6ef
yinyin13 <yinyin13@uw.edu>
2024-05-20 20:23:57 -0700

    update fetch function

--- hardware/intake.py
+++ hardware/intake.py
@@ -20,44 +20,6 @@ def fetch_daily_intake_schedule():
 
             date = str(datetime.now().date())
             query = '''
-            SELECT il."intakeTime", il."isIntaked", il."scheduleTime", il."scheduleDate", il."status", p."pillName", p."caseNo", p."doses"
-            FROM "IntakeLog" il
-            JOIN "Pillcase" p ON il."pillcaseId" = p.id
-            WHERE il."scheduleDate" = %s
-            '''
-            cursor.execute(query, (date,))
-            
-            columns = [desc[0] for desc in cursor.description if desc[0] != "pillcaseId"]
-            rows = cursor.fetchall()
-            schedule = [dict(zip(columns, row)) for row in rows]
-
-            return schedule
-
-    except Exception as error:
-        print("Error fetching daily intake schedule:", error)
-
-import os
-from datetime import datetime
-from dotenv import load_dotenv, find_dotenv
-from azure import connect_to_database
-
-# Load environment variables from .env file
-load_dotenv(find_dotenv())
-
-# Database connection information
-dbname = os.getenv("DATABASE")
-user = "chelsey"
-password = os.getenv("PASSWORD")
-host = os.getenv("HOST")
-port = os.getenv("PORT")
-
-def fetch_daily_intake_schedule():
-    try:
-        with connect_to_database() as conn:
-            cursor = conn.cursor()
-
-            date = str(datetime.now().date())
-            query = '''
             SELECT "intakeTime", "isIntaked", "scheduleTime", "scheduleDate", "status", "pillName", "caseNo", "doses"
             FROM "IntakeLog"
             WHERE "scheduleDate" = %s
