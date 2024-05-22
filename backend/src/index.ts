dabc0ec977c22875700c977b3aa7f39f3462bc91
Chelsey Lee <chelsey0527@gmail.com>
2024-05-21 19:33:41 -0700

    remove port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,14 +35,17 @@ const server = http.createServer(app);
 const webSocketService = new WebSocketService(server);
 
 // Setting up the cron job to run at midnight every day
-// var task = cron.schedule('0 0 0 * * *', () => {
-//     console.log('Running daily schedule initialization');
-//     scheduleDailyIntakeLogs("ee430f72-7def-434c-ade8-c464c04655b7"); // ** Modify user id stored in token
-// },
-//     {
-//         scheduled: true,
-//         timezone: "America/Los_Angeles"
-//     });
+var task = cron.schedule(
+  "0 0 0 * * *",
+  () => {
+    console.log("Running daily schedule initialization");
+    scheduleDailyIntakeLogs("ee430f72-7def-434c-ade8-c464c04655b7"); // ** Modify user id stored in token
+  },
+  {
+    scheduled: true,
+    timezone: "America/Los_Angeles",
+  }
+);
 
 // TEST: Setting up the cron job to every minutes
 // var testTask = cron.schedule(
